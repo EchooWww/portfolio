68d5bebc436276aa2610847992fdda7bda712eb1
Echo Wang <ywang623@my.bcit.ca>
2023-08-19 15:26:26 -0700

    Added skills section

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -8,7 +8,7 @@ import { useSectionInView } from "../lib/hooks";
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
   return (
-    <section className="scroll-mt-28" id="projects" ref={ref}>
+    <section className="scroll-mt-28 mb-28" id="projects" ref={ref}>
       <SectionHeading> My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
