4cc5c24f79ea9d93b6fb7feb15e655831189e957
Echo Wang <ywang623@my.bcit.ca>
2023-08-19 02:40:08 -0700

    Added intro section

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  compiler: {
-    styledComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imgur.com",
+      },
+    ],
   },
 };
